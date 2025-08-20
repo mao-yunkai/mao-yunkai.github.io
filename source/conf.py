7047b2a6d8a81f5c6faeecd8790bb20d7a7ca6e1
maoshuainan <maoshuainan@outlook.com>
2025-08-20 15:50:25 +0800

    添加工程文件到main分支

---
+++ source/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = '毛云开的知识库'
+copyright = '2025, Leo Mao'
+author = 'Leo Mao'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'myst_parser',
+    'sphinx.ext.githubpages'
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh-CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_book_theme'
+html_static_path = ['_static']
+
+myst_enable_extensions = ['dollarmath', 'amsmath']
+myst_dmath_double_inline = True
+myst_standalone_images = True
